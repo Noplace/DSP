8720af16704a830c77119f59750ce4934986b612
Khalid Al-Kooheji <khalid.alkooheji@gmail.com>
2014-04-08 07:13:25 +0300

    Initial

---
+++ DSP_Test/Code/global.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+#include <gl\gl.h>
+#include <gl\glext.h>
+#include <gl\wglext.h>
+#include "timer.h"
+#include "gl.h"
+#include "client_main_window.h"
+
